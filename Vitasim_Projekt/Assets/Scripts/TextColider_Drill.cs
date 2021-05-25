eafe832cb5d2e09f745e867afd11966ec22962ab
Frede Emil Baunsgaard Brandt <fred920e@edu.ucl.dk>
2021-05-25 15:36:10 +0200

    sd

--- Vitasim_Projekt/Assets/Scripts/TextColider_Drill.cs
+++ Vitasim_Projekt/Assets/Scripts/TextColider_Drill.cs
@@ -5,25 +5,23 @@ using UnityEngine.UI;
 
 public class TextColider_Drill : MonoBehaviour
 {
-    static bool FirstTrigger;
+    static bool FirstTrigger = true;
     [SerializeField]
     public Text text;
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (FirstTrigger == true)
+        if (FirstTrigger == true && other.tag == "Player")
         {
- 
-            text.color = new Color(0, 255, 0, 1);
-        
-            text.CrossFadeAlpha(0, 4, false);
-        
+            TextScripts.ChangeText(new Color(0, 255, 0, 1), 0, 4, "", text);
+            TextScripts.TextChanger(new Color(255, 255, 255, 1), 1, 0, "Turn on the machine before drilling", text)
+
             text.text = "Turn on the machine before drilling";
-        
+
             text.color = new Color(255, 255, 255, 1);
-        
+
             text.CrossFadeAlpha(1, 0, false);
             FirstTrigger = false;
+            TextScripts.guideStage = 1;
         }
-       
     }
 }
--- Vitasim_Projekt/Assets/Scripts/TextScripts.cs
+++ Vitasim_Projekt/Assets/Scripts/TextScripts.cs
@@ -9,7 +9,31 @@ public class TextScripts : MonoBehaviour
     static public int guideStage = 0;
     public static string currentMessage = "";
     //Used to change a text i varius  ways. and also plays a sound depending on the text color
-    public static void ChangeText(Color a, int alpha, float waittime, string message, Text textobject)
+    //public static void ChangeText(Color a, int alpha, float waittime, string message, Text textobject)
+    //{
+    //    SoundController sound = new SoundController();
+    //    if (a == new Color(0, 255, 0, 1))
+    //    {
+    //        sound.PlayAudioClip(true);
+    //    }
+    //    if (a == new Color(255, 0, 0, 1))
+    //    {
+    //        sound.PlayAudioClip(false);
+    //    }
+    //    else
+    //    {
+
+    //    }
+    //    textobject.color = a;
+    //    if (message == "")
+    //    { }
+    //    else
+    //    { textobject.text = message; }
+    //    textobject.CrossFadeAlpha(alpha, waittime, false);
+
+    //}
+
+    public static IEnumerator TextChanger(Color a, int alpha, float waittime, string message, Text textobject)
     {
         SoundController sound = new SoundController();
         if (a == new Color(0, 255, 0, 1))
@@ -20,16 +44,12 @@ public class TextScripts : MonoBehaviour
         {
             sound.PlayAudioClip(false);
         }
-        else
-        {
-
-        }
         textobject.color = a;
         if (message == "")
         { }
         else
         { textobject.text = message; }
         textobject.CrossFadeAlpha(alpha, waittime, false);
-
+        yield return new WaitForSeconds(waittime);
     }
 }
